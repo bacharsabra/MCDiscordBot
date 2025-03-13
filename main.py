731926eb50794883850b3e9eb6ed8f2024643ebd
Bachar Sabr <bacharsabra@hotmail.com>
2025-03-13 02:03:39 +0100

    Shortening interval

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def check_server_status():
         online = get_server_status()
         if online:
             print("✅ Server is ONLINE")
-            check_interval = 3600
+            check_interval = 60
             if last_status is False:
                 await channel.send("💡 Dar lserver! @everyone")
         else:
